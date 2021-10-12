f33b2ca9a310e377aa748477f493b02cae351a82
CarlosStrider120 <sherifinho.augusto@gmail.com>
2021-10-11 23:54:35 -0300

    Adicionando data nas tarefas com a biblioteca moment.js e separando o código em duas funções, uma para criar os objetos e a outra para adiciona-los

--- main.js
+++ main.js
@@ -1,23 +1,39 @@
 import BotaoConclui from "./componentes/concluiTarefa.js";
 import BotaoDeleta from "./componentes/deletaTarefa.js";
 
-const criarTarefa = (evento) => {
+const handleNovoItem = (evento) => {
     evento.preventDefault();
-    const lista = document.querySelector('[data-list]')
+
+    const lista = document.querySelector('[data-list]');
     const input = document.querySelector('[data-form-input]');
     const valor = input.value;
 
+    const calendario = document.querySelector('[data-form-date]');
+    const data = moment(calendario.value).format('DD/MM/YYYY');
+
+    const dados = {
+        valor,
+        data
+    }
+
+    const criaTarefa = criarTarefa(dados)
+
+    lista.appendChild(criaTarefa);
+    input.value = " ";
+}
+
+const criarTarefa = ({valor, data}) => {
     const tarefa = document.createElement('li');
     tarefa.classList.add('task');
-    const conteudo = `<p class="content">${valor}</p>`;
+    const conteudo = `<p class="content">${data} * ${valor}</p>`;
 
     tarefa.innerHTML = conteudo;
 
     tarefa.appendChild(BotaoConclui());
     tarefa.appendChild(BotaoDeleta());
-    lista.appendChild(tarefa);
-    input.value = " ";
+
+    return tarefa;
 }
 
 const novaTarefaBotao = document.querySelector('[data-form-button]');
-novaTarefaBotao.addEventListener('click', criarTarefa);
+novaTarefaBotao.addEventListener('click', handleNovoItem);
